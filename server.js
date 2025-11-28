bd95286db049c72edbbdca247cf777e3a5c42f21
Rob McCallum <robmccallum01@gmail.com>
2025-11-28 13:29:30 -0500

    update

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import handler from "./api/generate-client-report.js";
 
 console.log("Loaded key:", process.env.DOCRAPTOR_API_KEY);
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json({ limit: "10mb" }));
 
 // Connect your route
