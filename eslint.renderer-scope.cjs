a01175aaf40817cb1f69354771a9dbdf83bf1e70
Rob McCallum <robmccallum01@gmail.com>
2026-05-13 18:53:26 -0400

    update

---
+++ eslint.renderer-scope.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  rules: {
+    "no-undef": "error",
+  },
+};
