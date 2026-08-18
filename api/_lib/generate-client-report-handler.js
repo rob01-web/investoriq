1ff5148940bf3e0148cbf06bcb2b0e6747249f2b
rob01-web <robmccallum01@gmail.com>
2026-08-18 10:23:41 -0400

    fix: project typed canonical delivery action

--- api/_lib/generate-client-report-handler.js
+++ api/_lib/generate-client-report-handler.js
@@ -1,5 +1,8 @@
 import implHandler from "./generate-client-report-impl.js";
+import { attachCanonicalDeliveryAction } from "./canonical-delivery-action.js";
 
 export default async function handler(req, res) {
+  const originalJson = res.json.bind(res);
+  res.json = (payload) => originalJson(attachCanonicalDeliveryAction(payload));
   return implHandler(req, res);
 }
