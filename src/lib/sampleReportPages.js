75f3459f5277425c9fdc2cde114b734dd9814645
rob01-web <robmccallum01@gmail.com>
2026-08-18 10:55:50 -0400

    cleanup: remove empty obsolete sample report authority

--- src/lib/sampleReportPages.js
+++
@@ -1,6 +0,0 @@
-// Intended file naming (example):
-// /samples/screening/page-01.webp
-// /samples/underwriting/page-01.webp
-
-export const SCREENING_SAMPLE_PAGES = [];
-export const UNDERWRITING_SAMPLE_PAGES = [];
